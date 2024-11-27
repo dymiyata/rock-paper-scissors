1f103beb917e2fd0f1d892375ba4e1f2f47708dc
Dane Miyata <48339284+dymiyata@users.noreply.github.com>
2024-11-27 09:32:08 -0800

    Finish getComputerChoice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+function getComputerChoice() {
+  const randNum = 3 * Math.random();
+  if (randNum < 1) {
+    return 'rock';
+  }
+  if (randNum < 2) {
+    return 'paper';
+  }
+  return 'scissors';
+}
+
